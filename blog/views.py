3f17c1b798ee8c2e3131bbf53399c3cdd3fedcb8
belhajla mohamed <belhajlamohamed@gmail.com>
2022-08-04 17:54:51 +0100

    Post Model created and used

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
+from multiprocessing import context
 from django.shortcuts import render
+from .models import Post
+from django.views.generic.list import ListView
 
-# Create your views here.
+
+class PostListView(ListView):
+    model = Post 
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    
